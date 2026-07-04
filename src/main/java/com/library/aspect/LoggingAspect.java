c33239a7fad5106ccfcdef9daaaef948d788c1bd
ananyaissac01-729 <ananyaissac01@gmail.com>
2026-07-04 13:51:24 +0530

    Exercise 8

--- src/main/java/com/library/aspect/LoggingAspect.java
+++ src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,17 +1,30 @@
 package com.library.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
+
+    @Before("execution(* com.library.service.BookService.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Before executing: " + joinPoint.getSignature());
+    }
+
+    @After("execution(* com.library.service.BookService.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        System.out.println("After executing: " + joinPoint.getSignature());
+    }
 
     @Around("execution(* com.library.service.BookService.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
 
-        Object result = joinPoint.proceed(); // runs the actual method
+        Object result = joinPoint.proceed(); 
 
         long end = System.currentTimeMillis();
         System.out.println(joinPoint.getSignature() + " executed in " + (end - start) + "ms");
